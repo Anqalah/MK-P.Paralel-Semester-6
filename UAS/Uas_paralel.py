a02fa8d21bd06f5bdd085e97a211d1ae81b78df6
Anqalah <evodark7@gmail.com>
2023-06-15 15:46:21 +0800

    Upload UAS v3 Pemrograman Parallel Semester 6

--- UAS/Uas_paralel.py
+++ UAS/Uas_paralel.py
@@ -7,87 +7,94 @@ import threading
 import docx2pdf
 import time
 
-
+# Fungsi untuk memulai proses konversi
 def convert_to_pdf():
-    # Mendapatkan daftar file Word
-    file_paths = filedialog.askopenfilenames(filetypes=[("Word Documents", "*.docx")])
+    # Membuka dialog file untuk memilih dokumen Word
+    file_paths = filedialog.askopenfilenames(filetypes=[("Dokumen Word", "*.docx")])
 
     if len(file_paths) == 0:
         return
 
-    # Memilih folder tujuan untuk file PDF
+    # Membuka dialog folder untuk memilih folder tujuan penyimpanan file PDF
     output_folder = filedialog.askdirectory()
     if not output_folder:
         return
 
-    # Memulai thread untuk menjalankan konversi ke PDF
+    # Memulai thread untuk melakukan konversi ke PDF
     thread = threading.Thread(target=convert_files, args=(file_paths, output_folder))
     thread.start()
 
+# Fungsi untuk mengonversi beberapa file ke PDF
 def convert_files(file_paths, output_folder):
     try:
         total_files = len(file_paths)
         progress_value = 0
 
+        # Membuat jendela kemajuan
         progress_window = tk.Toplevel(root)
         progress_window.title("Proses Konversi")
         progress_window.geometry("300x100")
 
+        # Membuat label untuk jendela kemajuan
         progress_label = tk.Label(progress_window, text="Konversi sedang berlangsung...", font=("Poppins", 12))
         progress_label.pack(pady=10)
 
+        # Membuat progress bar
         progress_bar = Progressbar(progress_window, length=250, mode="determinate")
         progress_bar.pack(pady=10)
 
         for file_path in file_paths:
-            # Membuat thread untuk setiap file Word
+            # Memulai thread untuk mengonversi setiap file Word
             thread = threading.Thread(target=convert_file, args=(file_path, output_folder))
             thread.start()
 
-            thread.join()
+            thread.join()  # Menunggu thread konversi selesai
             progress_value += 1
             progress_bar["value"] = (progress_value / total_files) * 100
             progress_window.update()
 
-            # Menambahkan penundaan selama 1 detik untuk efek thread
-            time.sleep(1)
+            # Menambahkan jeda selama 1 detik untuk efek thread
+            time.sleep(2)
 
-        # Menampilkan pesan berhasil dalam kotak dialog
+        # Menampilkan kotak dialog pesan berhasil
         messagebox.showinfo("Konversi Berhasil", "File berhasil dikonversi ke PDF.")
 
-        # Menampilkan folder tujuan dalam kotak dialog
+        # Menampilkan path folder tujuan dalam kotak dialog
         messagebox.showinfo("Folder Tujuan", f"File PDF disimpan di:\n{output_folder}")
 
         progress_window.destroy()
     except Exception as e:
-        # Menampilkan pesan error dalam kotak dialog
+        # Menampilkan kotak dialog pesan error jika terjadi exception
         messagebox.showerror("Konversi Gagal", str(e))
         progress_window.destroy()
 
+# Fungsi untuk mengonversi satu file menjadi PDF
 def convert_file(file_path, output_folder):
     try:
-        # Mengonversi file Word ke PDF
+        # Mengonversi file Word menjadi PDF
         pdf_path = os.path.join(output_folder, os.path.basename(file_path) + ".pdf")
         docx2pdf.convert(file_path, pdf_path)
     except Exception as e:
-        # Menampilkan pesan error dalam kotak dialog
+        # Menampilkan kotak dialog pesan error jika terjadi exception
         messagebox.showerror("Konversi Gagal", str(e))
 
-# Membuat GUI dengan menggunakan Tkinter
+# Membuat GUI menggunakan Tkinter
 root = tk.Tk()
 root.title("Konversi Word ke PDF")
 root.geometry("400x200")
 root.configure(bg="#000000")
 
-# Membuat label judul
+# Membuat label untuk judul
 title_label = tk.Label(root, text="Konversi File Word ke PDF", font=("Poppins", 16), bg="black", fg="orange")
 title_label.pack(pady=10)
+
+# Membuat label untuk subjudul
 subtitle_label = tk.Label(root, text="UAS P.Paralel Kelompok 7", font=("Poppins", 16), bg="black", fg="orange")
 subtitle_label.pack(pady=10)
 
-# Membuat tombol "Pilih File" untuk memilih file Word dan konversi ke PDF
+# Membuat tombol untuk memilih file Word dan mengonversinya ke PDF
 select_button = tk.Button(root, text="Pilih File Word", font=("Arial", 12), command=convert_to_pdf, bg='#A2B38B')
 select_button.pack(pady=20)
 
-# Menjalankan mainloop GUI
+# Menjalankan event loop GUI
 root.mainloop()
